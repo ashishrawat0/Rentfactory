8dcef70bf9f0f174a38b40fb13564be5ced2d370
Lakshya Kalia <lkalia.cse16@chitkarauniversity.edu.in>
2020-05-10 00:35:45 +0530

    footer component made

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
